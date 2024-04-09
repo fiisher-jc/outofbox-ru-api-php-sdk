abf7eadfa34a03517e178bcc19032c48b080e916
Yury Karpov <yura@japancar.ru>
2024-04-09 11:01:30 +1000

    получение объекта денормализации по другому

--- src/Serializer/ProductDenormalizer.php
+++ src/Serializer/ProductDenormalizer.php
@@ -12,11 +12,14 @@ use Symfony\Component\Serializer\Normalizer\DenormalizerAwareInterface;
 use Symfony\Component\Serializer\Normalizer\DenormalizerAwareTrait;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectToPopulateTrait;
 
 #[AllowDynamicProperties]
 //class ProductDenormalizer extends ObjectNormalizer
-class ProductDenormalizer implements DenormalizerInterface, LoggerAwareInterface
+class ProductDenormalizer implements DenormalizerInterface, DenormalizerAwareInterface, LoggerAwareInterface
 {
+    use DenormalizerAwareTrait;
+    use ObjectToPopulateTrait;
     use LoggerAwareTrait;
 
     /**
@@ -24,16 +27,22 @@ class ProductDenormalizer implements DenormalizerInterface, LoggerAwareInterface
      */
     public function denormalize($data, $type, $format = null, array $context = []): mixed
     {
-        $odn = new ObjectNormalizer(null, null, null, new PhpDocExtractor());
+        //$odn = new ObjectNormalizer(null, null, null, new PhpDocExtractor());
 
         /** @var Product $product */
         //$product = parent::denormalize($data, $type, $format, $context);
         $this->logger?->debug('ProductDenormalizer: start dernomalize');
         try {
             //$this->logger?->debug(get_class($this->denormalizer));
-            $this->logger?->debug('data : '. var_export($data, true));
+            $this->logger?->debug('DATA : '. var_export($data, true));
+            $this->logger?->debug('TYPE : '. var_export($type, true));
+            $this->logger?->debug('FORMAT : '. var_export($format, true));
+            $this->logger?->debug('CONTEXT : '. var_export($context, true));
+
+            $product = $this->extractObjectToPopulate($type, $context) ?? new $type();
+            $product->denormalize($this->denormalizer,$data, $format, $context);
             //$product = $this->denormalizer->denormalize($data, $type, $format, $context);
-            $product = $odn->denormalize($data, $type, $format, $context);
+            //$product = $odn->denormalize($data, $type, $format, $context);
         } catch (\Throwable $exception) {
             $this->logger?->debug('ProductDenormalizer: denormalize error: '.$exception->getMessage());
         }
